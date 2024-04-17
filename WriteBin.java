d77ec849c3219f7e949f59d31a4ac59d3de1723f
piecucci <piecucci@gmail.com>
2024-04-17 16:28:42 +0200

    VS Code Commit/Sync

--- WriteBin.java
+++ WriteBin.java
@@ -1,5 +1,7 @@
 // KEP-WriteBin/WriteBin.java
- 
+// Listing 75
+// Page 117
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,28 +9,24 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class WriteBin {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         long before = -284803830071168L;
         System.out.println("before = " + before);
         try (
-            OutputStream os =
-                    new FileOutputStream("WriteBin.bin");
-        ) {
+                OutputStream os = new FileOutputStream("WriteBin.bin");) {
             for (int i = 7; i >= 0; --i)
-                os.write( (int)(before >> i*8) );
-        } catch(IOException e) {
+                os.write((int) (before >> i * 8));
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
 
         long after = 0;
         try (
-            InputStream is =
-                    new FileInputStream("WriteBin.bin");
-        ) {
+                InputStream is = new FileInputStream("WriteBin.bin");) {
             for (int i = 0; i < 8; ++i)
                 after = (after << 8) | is.read();
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
